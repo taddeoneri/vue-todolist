a873b4a084c8445bde4de86cad2d7b31eabcd8fd
taddeoneri <taddeo.neri10@gmail.com>
2023-03-30 15:36:26 +0200

    init lista spesa

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/*
+MILESTONE 1
+Stampare all'interno di una lista HTML un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+*/
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            listaSpesa: [
+                {
+                    nomeProdotto: 'pasta',
+                    comprato: false 
+                },
+                {
+                    nomeProdotto: 'pane',
+                    comprato: false 
+                },
+                {
+                    nomeProdotto: 'insalta',
+                    comprato: false 
+                },
+                {
+                    nomeProdotto: 'torta',
+                    comprato: false 
+                },
+                {
+                    nomeProdotto: 'acqua',
+                    comprato: false 
+                }
+            ]
+        }
+    },
+    methods: {
+        inserisciProdotti(){
+            
+        }
+    }
+}).mount('#app');
